ad2c918a7f90dc7562a3b82b1fce4d9da2233733
m-oooooonchild <syp614@ewhain.net>
2024-11-01 11:40:31 +0900

    💄 상품 등록 완료 페이지 퍼블리싱

--- src/pages/Sale/ProductRegistCompletePage/ProductRegistCompletePage.tsx
+++ src/pages/Sale/ProductRegistCompletePage/ProductRegistCompletePage.tsx
@@ -1,5 +1,22 @@
+import * as S from './ProductRegistCompletePage.style';
+import { ReactComponent as Logo } from '../../../assets/ProductRegistPage/karma-logo.svg';
+import { Button } from '../../../components/common/Button';
+import { useNavigate } from 'react-router-dom';
+
 const ProductRegistCompletePage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+    <S.Container>
+      <S.Title>상품이 등록되었습니다!</S.Title>
+      <S.LogoWrapper>
+        <Logo />
+      </S.LogoWrapper>
+      <S.ButtonWrapper onClick={() => navigate('/')}>
+        <Button isActive={true} text='돌아가기' color='white' />
+      </S.ButtonWrapper>
+    </S.Container>
+  );
 };
 
 export default ProductRegistCompletePage;
