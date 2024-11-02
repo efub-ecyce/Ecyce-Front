7f7b36a771bef01c20715e4c2b9a01f2b66445c5
m-oooooonchild <syp614@ewhain.net>
2024-11-03 01:42:03 +0900

    💄 알림페이지 퍼블리싱

--- src/pages/Main/NotificationPage/NotificationPage.tsx
+++ src/pages/Main/NotificationPage/NotificationPage.tsx
@@ -1,5 +1,41 @@
+import * as S from './NotificationPage.style';
+import Header from '../../../components/common/Header';
+import NavBar from '../../../components/common/NavBar';
+import { Noti } from '../../../components/NotificationPage/Noti';
+
 const NotificationPage = () => {
-  return <div></div>;
+  return (
+    <S.Container>
+      <Header title='알림' />
+      <S.NotiContainer>
+        <Noti
+          isRead={false}
+          type='ship'
+          title={'상품 배송이 시작됐어요!'}
+          subtitle={'청바지 텀블러 가방'}
+          time={'5분 전'}
+        />
+        <Noti
+          isRead={true}
+          type='box'
+          title={'상품을 제작하고 있어요!'}
+          subtitle={'청바지 텀블러 가방'}
+          time={'7일 전'}
+        />
+
+        <Noti
+          isRead={true}
+          type='check'
+          title={'판매자가 주문을 수락했어요!'}
+          subtitle={'청바지 텀블러 가방'}
+          time={'24 / 09 / 26'}
+        />
+      </S.NotiContainer>
+      <S.NavWrapper>
+        <NavBar />
+      </S.NavWrapper>
+    </S.Container>
+  );
 };
 
 export default NotificationPage;
