dc1e3fae51a73205bea678affdb89dc65788f034
m-oooooonchild <syp614@ewhain.net>
2024-11-06 23:54:07 +0900

    💄 메시지 그룹화

--- src/pages/Chat/ChatPage/ChatPage.style.ts
+++ src/pages/Chat/ChatPage/ChatPage.style.ts
@@ -38,61 +38,6 @@ export const Name = styled.div`
   margin-left: 0.55rem;
 `;
 
-export const MessageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-
-  width: 100%;
-  height: calc(100vh - 10rem);
-  padding: 0.5rem 1.25rem 0;
-  box-sizing: border-box;
-
-  overflow-y: auto;
-
-  &::-webkit-scrollbar {
-    display: none; /* Chrome, Safari, Edge */
-  }
-`;
-
-export const MyChat = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-
-  max-width: 75%;
-  margin-left: auto;
-  padding: 0.5rem 0.875rem;
-  box-sizing: border-box;
-
-  background-color: var(--mint02);
-  border-radius: 0.9375rem;
-  box-shadow: 0px 0px 5px 0px rgba(61, 73, 70, 0.1);
-
-  ${font.regular}
-  color: var(--white00);
-  font-size: 1rem;
-`;
-
-export const YourChat = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-
-  max-width: 75%;
-  margin-right: auto;
-  padding: 0.5rem 0.875rem;
-  box-sizing: border-box;
-
-  background-color: var(--white01);
-  border-radius: 0.9375rem;
-  box-shadow: 0px 0px 5px 0px rgba(61, 73, 70, 0.1);
-
-  ${font.regular}
-  color: var(--black);
-  font-size: 1rem;
-`;
-
 export const InputBar = styled.div`
   display: flex;
   justify-content: space-between;
--- src/pages/Chat/ChatPage/ChatPage.tsx
+++ src/pages/Chat/ChatPage/ChatPage.tsx
@@ -5,6 +5,47 @@ import { ReactComponent as MoreIcon } from '../../../assets/ChatPage/more_vert.s
 import { ReactComponent as SendIcon } from '../../../assets/ChatPage/send.svg';
 import { ChatModal } from '../../../components/ChatPage/ChatModal';
 import { useState } from 'react';
+import { MessageList } from '../../../components/ChatPage/MessageList';
+import { timeStamp } from 'console';
+
+interface Message {
+  senderId: number;
+  text: string;
+  timestamp: string; //추후 수정 (format 함수 만들기..)
+}
+
+const DummyMessages: Message[] = [
+  {
+    senderId: 0,
+    text: '안녕하세요',
+    timestamp: '10:00',
+  },
+  {
+    senderId: 1,
+    text: '네 안녕하세요',
+    timestamp: '10:00',
+  },
+  {
+    senderId: 1,
+    text: '내 식탁에 누가 손을 올려 감히 내 허락이 없이는 Ain’t nobody 원하는 건 다 내 손안에 Get it 뒤로 기대 Lean 떨어 떨어 다리 털끝 하나 건들다간 Runnin’ like a dog, shhh Face down Don’t get caught, jeez 난 뺏긴 적이 없지 매번 독식 그저 Teeth and claw로 Notice',
+    timestamp: '10:00',
+  },
+  {
+    senderId: 0,
+    text: 'Yeah don’t you dare dare dare 선을 넘는 순간 Maybe just one time 무너뜨려 Knock down It’s so perfect 커져 가는 이 Thrillin’ And you know that you’re feedin’ my appetite',
+    timestamp: '10:02',
+  },
+  {
+    senderId: 1,
+    text: '난 입을 벌렸다면 I bite that If you take a bite then I Bite Back Bite Back Bite Back Bite Back Bite Back',
+    timestamp: '10:08',
+  },
+  {
+    senderId: 0,
+    text: '내 것을 탐한다면 (Uh) I like that (I like that) If you take a bite then I Bite Back Bite Back Bite Back Bite Back Bite Back',
+    timestamp: '10:08',
+  },
+];
 
 const ChatPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -32,29 +73,7 @@ const ChatPage = () => {
         </S.IconWrapper>
         {isModalOpen && <ChatModal />}
       </S.Header>
-      <S.MessageContainer>
-        <S.MyChat>안녕하세요</S.MyChat>
-        <S.YourChat>네 안녕하세요</S.YourChat>
-        <S.YourChat>
-          내 식탁에 누가 손을 올려 감히 내 허락이 없이는 Ain’t nobody 원하는 건
-          다 내 손안에 Get it 뒤로 기대 Lean 떨어 떨어 다리 털끝 하나 건들다간
-          Runnin’ like a dog, shhh Face down Don’t get caught, jeez 난 뺏긴 적이
-          없지 매번 독식 그저 Teeth and claw로 Notice
-        </S.YourChat>
-        <S.MyChat>
-          Yeah don’t you dare dare dare 선을 넘는 순간 Maybe just one time
-          무너뜨려 Knock down It’s so perfect 커져 가는 이 Thrillin’ And you
-          know that you’re feedin’ my appetite
-        </S.MyChat>
-        <S.YourChat>
-          난 입을 벌렸다면 I bite that If you take a bite then I Bite Back Bite
-          Back Bite Back Bite Back Bite Back
-        </S.YourChat>
-        <S.MyChat>
-          내 것을 탐한다면 (Uh) I like that (I like that) If you take a bite
-          then I Bite Back Bite Back Bite Back Bite Back Bite Back
-        </S.MyChat>
-      </S.MessageContainer>
+      <MessageList messages={DummyMessages} />
       <S.InputBar>
         <S.IconWrapper>
           <AddIcon />
