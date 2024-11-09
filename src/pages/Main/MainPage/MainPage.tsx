da732ec5dbe57b07df6a93a163982d1dd40d6796
Gabin Lee <gb94@ewhain.net>
2024-11-09 22:35:28 +0900

    ✨ 메인 페이지 필터링 컴포넌트 구현

--- src/pages/Main/MainPage/MainPage.tsx
+++ src/pages/Main/MainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import NavBar from '../../../components/common/NavBar';
 import ProductComponent1 from '../../../components/common/ProductComponent1';
 import Header from '../../../components/MainPage/Header';
+import Filter from '../../../components/MainPage/Filter';
 
 const MainPage = () => {
   const productData = {
@@ -14,6 +15,7 @@ const MainPage = () => {
   return (
     <div>
       <Header />
+      <Filter />
       <ProductComponent1
         title={productData.title}
         term={productData.term}
