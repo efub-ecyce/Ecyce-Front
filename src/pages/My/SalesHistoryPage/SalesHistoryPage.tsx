7fb68b43bbd0fca74a1525e210328dc8eb57a935
m-oooooonchild <syp614@ewhain.net>
2024-11-22 18:02:33 +0900

    ✨ 판매 내역 페이지 api 연결

--- src/pages/My/SalesHistoryPage/SalesHistoryPage.tsx
+++ src/pages/My/SalesHistoryPage/SalesHistoryPage.tsx
@@ -1,26 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from './SalesHistoryPage.style';
 import Header from '../../../components/common/Header';
 import { FilterBar } from '../../../components/HistoryPage/FilterBar';
 import { DatePickerBar } from '../../../components/HistoryPage/DatePickerBar';
 import { History } from '../../../components/HistoryPage/History';
 import NavBar from '../../../components/common/NavBar';
+import { HistoryProps } from '../PurchaseHistoryPage/PurchaseHistoryPage';
+import { getSalesHistory } from '../../../api/order';
 
 const filterList = ['전체', '접수', '진행중', '완료'];
 
-const dummyHistory = {
-  orderId: 1,
-  date: '2024.11.08',
-  state: '접수 완료',
-  image: undefined,
-  name: '텀블러 가방',
-  options: {
-    옵션1: 1,
-    옵션2: 133,
-    옵션3: 144,
+const dummyHistory = [
+  {
+    orderId: 1,
+    productName: '파우치 가방 리사이클링',
+    productOption: '텀블러 옵션 1',
+    orderCount: 1,
+    orderState: '접수완료',
+    createdAt: '2024-11-08T22:35:53.26533',
+    totalPrice: 15000, // 배송비 + 상품가격
+    productImages: {
+      productImageId: 1,
+      productImageUrl:
+        'https://karma-s3-bucket.s3.ap-northeast-2.amazonaws.com/images/reviewImages/example1.jpg',
+    },
   },
-  price: 40000,
-};
+  {
+    orderId: 1,
+    productName: '파우치 가방 리사이클링',
+    productOption: '텀블러 옵션 1',
+    orderCount: 1,
+    orderState: '접수완료',
+    createdAt: '2024-11-08T22:35:53.26533',
+    totalPrice: 15000, // 배송비 + 상품가격
+    productImages: {
+      productImageId: 1,
+      productImageUrl:
+        'https://karma-s3-bucket.s3.ap-northeast-2.amazonaws.com/images/reviewImages/example1.jpg',
+    },
+  },
+];
 
 const SalesHistoryPage = () => {
   const [activeFilter, setActiveFilter] = useState<string>(filterList[0]);
@@ -32,6 +51,44 @@ const SalesHistoryPage = () => {
 
   const [startDate, setStartDate] = useState<Date | null>(new Date());
   const [endDate, setEndDate] = useState<Date | null>(new Date());
+
+  const [historyList, setHistoryList] = useState<HistoryProps[]>([]);
+  const [filteredList, setFilteredList] = useState<HistoryProps[]>([]);
+
+  useEffect(() => {
+    const getHistoryList = async () => {
+      try {
+        const response = await getSalesHistory();
+        setHistoryList(response);
+
+        if (historyList.length == 0) {
+          setHistoryList(dummyHistory);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getHistoryList();
+  }, []);
+
+  useEffect(() => {
+    const filteredList = historyList.filter(history => {
+      if (activeFilter === '접수') {
+        return ['접수완료'].includes(history.orderState);
+      } else if (activeFilter === '진행중') {
+        return ['제작대기', '제작중', '제작완료', '배송중'].includes(
+          history.orderState,
+        );
+      } else if (activeFilter === '완료') {
+        return ['구매확정', '주문취소', '주문거절'].includes(
+          history.orderState,
+        );
+      }
+      return true; // 다른 필터의 경우 모든 상품 표시
+    });
+    setFilteredList(filteredList);
+  }, [activeFilter, historyList]);
 
   return (
     <>
@@ -48,7 +105,11 @@ const SalesHistoryPage = () => {
           setStartDate={setStartDate}
           setEndDate={setEndDate}
         />
-        <S.HistoryContainer></S.HistoryContainer>
+        <S.HistoryContainer>
+          {filteredList.map((item, idx) => (
+            <History key={idx} {...item} />
+          ))}
+        </S.HistoryContainer>
       </S.Container>
       <S.NavWrapper>
         <NavBar />
