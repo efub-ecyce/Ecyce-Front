88c4e55e0cd1b4dd9e7a6d46c3e446e4c3e68211
m-oooooonchild <syp614@ewhain.net>
2024-11-24 17:14:13 +0900

    ✨ 리뷰 작성 api 연결

--- src/api/review.ts
+++ src/api/review.ts
@@ -1,1 +1,29 @@
-export {};
+import { client } from './client';
+
+export interface ReviewProps {
+  orderId: number | undefined;
+  content: string;
+  rating: number;
+}
+
+export const postReview = async (
+  reviewData: ReviewProps,
+  reviewImages: File[],
+) => {
+  try {
+    const formData = new FormData();
+    Object.entries(reviewData).forEach(([key, value]) =>
+      formData.append(key, String(value)),
+    );
+    if (reviewImages && reviewImages.length > 0) {
+      reviewImages.forEach(item => formData.append('reviewImages', item)); // 나중에 키 확인
+    }
+
+    const res = await client.post(`/reviews`, formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
