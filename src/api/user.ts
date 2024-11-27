0c0006446fc2b9e91e7616b5d890dfc681542994
m-oooooonchild <syp614@ewhain.net>
2024-11-27 21:17:46 +0900

    ✨ 회원가입 api 확인 (사진x)

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,6 @@
 import { client } from './client';
 
-interface userInfo {
+interface UserInfo {
   name: string;
   nickname: string;
   phoneNumber: string;
@@ -10,7 +10,7 @@ interface userInfo {
 }
 
 export const postNewUser = async (
-  userInfo: userInfo,
+  userInfo: UserInfo,
   imageFile: File | null,
 ) => {
   const formData = new FormData();
@@ -18,15 +18,28 @@ export const postNewUser = async (
     formData.append('profileImage', imageFile);
   }
   Object.entries(userInfo).forEach(([key, value]) => {
+    if (key == 'postcode') {
+      formData.append('postalCode', value);
+    }
     formData.append(key, value);
   });
 
   try {
-    const res = await client.post(`/user`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
+    // const res = await client.post(`/user`, formData, {
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data',
+    //   },
+    // });
+
+    const tempData = {
+      name: userInfo.name,
+      nickname: userInfo.nickname,
+      phoneNumber: userInfo.phoneNumber,
+      postalCode: userInfo.postcode,
+      address1: userInfo.address1,
+      address2: userInfo.address2,
+    };
+    const res = await client.post(`/user`, tempData);
     return res.data;
   } catch (error) {
     throw error;
@@ -36,6 +49,32 @@ export const postNewUser = async (
 export const getUserInfo = async () => {
   try {
     const res = await client.get('/user');
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const patchUserInfo = async (
+  userInfo: UserInfo,
+  imageFile: File | null,
+) => {
+  try {
+    const formData = new FormData();
+    if (imageFile) {
+      formData.append('profileImage', imageFile);
+    }
+    Object.entries(userInfo).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    // const res = await client.patch('/user', formData, {
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data',
+    //   },
+    // });
+    const res = await client.patch('/user', userInfo);
+
     return res.data;
   } catch (error) {
     throw error;
