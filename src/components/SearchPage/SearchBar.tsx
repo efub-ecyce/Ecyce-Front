ac74cdf8a0bf6565b6e945c31e7cc4bd22f02823
Gabin Lee <gb94@ewhain.net>
2024-11-30 20:23:42 +0900

    ✨ 검색페이지 API 연결

--- src/components/SearchPage/SearchBar.tsx
+++ src/components/SearchPage/SearchBar.tsx
@@ -1,11 +1,29 @@
+import React, { useState } from 'react';
 import * as S from './SearchBar.style';
 import { ReactComponent as BackBtn } from '../../assets/common/back_btn.svg';
 
-const SearchBar = () => {
+interface SearchBarProps {
+    onSearch: (searchWord: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setInputValue(value);
+        onSearch(value);
+    };
+    
     return (
         <S.Wrapper>
         <S.Btn><BackBtn /></S.Btn>
-        <S.SearchWindow type="text" placeholder="검색어를 입력하세요" />
+        <S.SearchWindow 
+            type="text" 
+            placeholder="검색어를 입력하세요"
+            value={inputValue}
+            onChange={handleInputChange}
+        />
         </S.Wrapper>
     );
 };
