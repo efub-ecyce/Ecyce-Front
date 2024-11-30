6ff99e067d8f76a81f8171948763a135046f7134
Gabin Lee <gb94@ewhain.net>
2024-11-30 16:26:27 +0900

    ✨ 상품 카테고리별 api 연결

--- src/pages/Main/MainPage/MainPage.tsx
+++ src/pages/Main/MainPage/MainPage.tsx
@@ -4,7 +4,7 @@ import ProductComponent1 from '../../../components/common/ProductComponent1';
 import Header from '../../../components/MainPage/Header';
 import Filter from '../../../components/MainPage/Filter';
 import * as S from './MainPage.style';
-import { getAllProduct } from '../../../api/product';
+import { getAllProduct, getProductCategory } from '../../../api/product';
 
 export interface ProductProps {
   key: number;
@@ -31,30 +31,44 @@ const productDummyData: ProductProps[] = [
 
 const MainPage = () => {
   const [productList, setProductList] = useState<ProductProps[]>([]);
+  const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
+
+  const fetchProductList = async (filterName: string | null) => {
+    try {
+      if (!filterName) {
+        // 기본값 == 모든 제품
+        const response = await getAllProduct();
+        setProductList(response.length ? response : productDummyData);
+      } else {
+        // 필터가 선택된 경우
+        const filterMap: { [key: string]: number } = {
+          후기순: 1,
+          북마크순: 2,
+          최신순: 3,
+        };
+        const categoryId = filterMap[filterName];
+        const response = await getProductCategory(categoryId);
+        setProductList(response.length ? response : productDummyData);
+      }
+    } catch (error) {
+      console.error(error);
+      setProductList(productDummyData);
+    }
+  };
 
   useEffect(() => {
-    const getProductList = async () => {
-      try {
-        const response = await getAllProduct();
-        if (!response || response.length === 0) {
-          setProductList(productDummyData);
-        } else {
-          setProductList(response);
-        }
-      } catch (error) {
-        console.error(error);
-        setProductList(productDummyData);
-      }
-    };
-  
-    getProductList();
-  }, []);
+    fetchProductList(selectedFilter);
+  }, [selectedFilter]);
+
+  const handleFilterChange = (filterName: string) => {
+    setSelectedFilter(filterName);
+  };
 
   return (
     <S.Container>
       <S.Top>
         <Header />
-        <Filter />
+        <Filter onFilterChange={handleFilterChange} />
       </S.Top>
       <S.Contents>
         {productList.map((product, index) => (
