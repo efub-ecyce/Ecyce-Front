a754e7c0c4c20e1038099208ca09978e50448817
Gabin Lee <gb94@ewhain.net>
2024-11-30 16:51:56 +0900

    ✨ 작가 홈 페이지 기본 정보 api 연결

--- src/api/artist.ts
+++ src/api/artist.ts
@@ -1,1 +1,10 @@
-export {};
+import { client } from './client';
+
+export const getArtistInfo = async (userId: number) => {
+    try {
+        const res = await client.get(`/artist/${userId}`);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
