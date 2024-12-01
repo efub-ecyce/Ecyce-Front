4114d37cf054a33c0358e957a0dc89f9639d9034
m-oooooonchild <syp614@ewhain.net>
2024-12-01 17:53:06 +0900

    ✨ 구매/판매내역 날짜 & 로딩 수정

--- src/pages/My/SalesDetailPage/SalesDetailPage.tsx
+++ src/pages/My/SalesDetailPage/SalesDetailPage.tsx
@@ -67,13 +67,17 @@ const SalesDetailPage = () => {
       <S.HeaderWrapper>
         <Header title='판매 내역' />
       </S.HeaderWrapper>
-      <SectionOne />
-      <S.Bar />
-      <SectionTwo />
-      <S.Bar />
-      <SectionThree />
-      <S.Bar />
-      <SectionFour />
+      {typeof salesDetail.orderId === 'number' && (
+        <>
+          <SectionOne />
+          <S.Bar />
+          <SectionTwo />
+          <S.Bar />
+          <SectionThree />
+          <S.Bar />
+          <SectionFour />
+        </>
+      )}
     </S.Container>
   );
 };
