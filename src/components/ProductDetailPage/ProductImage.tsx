1578f363e18ddc2d1515a606154a1ab8a7bbd6b7
Gabin Lee <gb94@ewhain.net>
2024-12-02 22:45:44 +0900

    🔧 상품 상세 페이지 뒤로가기 버튼

--- src/components/ProductDetailPage/ProductImage.tsx
+++ src/components/ProductDetailPage/ProductImage.tsx
@@ -1,6 +1,7 @@
 import * as S from './ProductImage.style'
 import { ReactComponent as GoBack } from '../../assets/ProductDetailPage/arrow_key_left.svg'
 import { ReactComponent as Share } from '../../assets/ProductDetailPage/share.svg'
+import { useNavigate } from 'react-router-dom'
 
 interface ReviewProps {
     reviewImg1: string;
@@ -11,13 +12,17 @@ interface ReviewProps {
 }
 
 const ProductImage = ({ reviewImg1, reviewImg2, reviewImg3, reviewImg4, reviewImg5}: ReviewProps) => {
+    const navigate = useNavigate();
+
     return (
         <S.Container>
             <S.ImgContainer>
                 {reviewImg1 ? <img src={reviewImg1} alt="Review Image 1" /> : null}
             </S.ImgContainer>
             <S.Top>
-                <GoBack />
+                <S.BackBtn onClick={() => navigate(-1)}>
+                    <GoBack />
+                </S.BackBtn>
                 <Share />
             </S.Top>
         </S.Container>
