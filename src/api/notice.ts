ef74bf6e9b208f4dac759df0821de9fdea0eb135
m-oooooonchild <syp614@ewhain.net>
2024-12-03 01:21:52 +0900

    ✨ 알림 api 연결

--- src/api/notice.ts
+++ src/api/notice.ts
@@ -1,1 +1,19 @@
-export {};
+import { client } from './client';
+
+export interface notiProps {
+  userId: number;
+  orderId: number;
+  productName: string;
+  orderState: string;
+  noticeContent: string;
+  updatedAt: string;
+}
+
+export const getAlertList = async () => {
+  try {
+    const res = await client.get('/notice');
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/pages/Main/NotificationPage/NotificationPage.tsx
+++ src/pages/Main/NotificationPage/NotificationPage.tsx
@@ -2,35 +2,39 @@ import * as S from './NotificationPage.style';
 import Header from '../../../components/common/Header';
 import NavBar from '../../../components/common/NavBar';
 import { Noti } from '../../../components/NotificationPage/Noti';
+import { useEffect, useRef, useState } from 'react';
+import { getAlertList, notiProps } from '../../../api/notice';
+import { useResetRecoilState } from 'recoil';
+import { PurchaseDetailState } from '../../My/PurchaseDetailPage/PurchaseDetailPage';
+import { SalesDetailState } from '../../My/SalesDetailPage/SalesDetailPage';
 
 const NotificationPage = () => {
+  const [notiList, setNotiList] = useState<notiProps[]>([]);
+  const resetPurchaseState = useResetRecoilState(PurchaseDetailState);
+  const resetSalesState = useResetRecoilState(SalesDetailState);
+
+  useEffect(() => {
+    const getNotiList = async () => {
+      try {
+        const res = await getAlertList();
+        setNotiList(res);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getNotiList();
+    resetPurchaseState();
+    resetSalesState();
+  }, []);
+
   return (
     <>
       <S.Container>
         <Header title='알림' />
         <S.NotiContainer>
-          <Noti
-            isRead={false}
-            type='ship'
-            title={'상품 배송이 시작됐어요!'}
-            subtitle={'청바지 텀블러 가방'}
-            time={'5분 전'}
-          />
-          <Noti
-            isRead={true}
-            type='box'
-            title={'상품을 제작하고 있어요!'}
-            subtitle={'청바지 텀블러 가방'}
-            time={'7일 전'}
-          />
-
-          <Noti
-            isRead={true}
-            type='check'
-            title={'판매자가 주문을 수락했어요!'}
-            subtitle={'청바지 텀블러 가방'}
-            time={'24 / 09 / 26'}
-          />
+          {notiList.map(item => (
+            <Noti {...item} />
+          ))}
         </S.NotiContainer>
       </S.Container>
       <S.NavWrapper>
