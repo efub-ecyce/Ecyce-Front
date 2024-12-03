4d63bb6bf93de0e1038045870319892f5292c774
m-oooooonchild <syp614@ewhain.net>
2024-12-03 13:15:23 +0900

    💄 그리드 반응형 수정

--- src/pages/Main/MainPage/MainPage.style.ts
+++ src/pages/Main/MainPage/MainPage.style.ts
@@ -1,56 +1,57 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #fafafb;
+  width: 100%;
+  height: 100vh;
+  background-color: #fafafb;
 
-    @media only screen and (min-width: 768px) {
-        width: 768px;
-        margin: 0 auto;
-    }
+  @media only screen and (min-width: 768px) {
+    width: 768px;
+    margin: 0 auto;
+  }
 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 
-    padding: 0 1.28125rem;
-    box-sizing: border-box;
+  padding: 0 1.28125rem;
+  box-sizing: border-box;
 
-    position: relative;
+  position: relative;
 `;
 
 export const Top = styled.div`
-    width: 100%;
-    position: absolute;
-    top: 0;
-    flex-shrink: 0;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  flex-shrink: 0;
 `;
 
 export const NavBar = styled.div`
-    width: 100%;
-    position: absolute;
-    bottom: 0;
+  width: 100%;
+  position: fixed;
+  bottom: -1px;
 `;
 
 export const Contents = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
-    gap: 1rem;
-    width: 100%;
-    min-height: 850px;
-    justify-content: center;
-    overflow-y: auto;
-    margin-top: calc(8rem);
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(calc(50% - 1.3rem), 1fr));
+  justify-items: center;
+  gap: 1rem;
+  width: 100%;
+  justify-content: center;
+  overflow-y: auto;
+  padding-top: 7rem;
+  padding-bottom: 6rem;
 
-    scrollbar-width: none;
-    -ms-overflow-style: none;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
 
-    &::-webkit-scrollbar {
-        display: none;
-    }
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
-    align-content: start;
+  align-content: start;
 `;
