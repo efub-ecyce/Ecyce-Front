c14b07b5fb186776ca29afa167972a6f5c47ee77
m-oooooonchild <syp614@ewhain.net>
2024-12-03 13:59:12 +0900

    💄 결제페이지 스타일링 수정

--- src/components/PaymentPage/DeliverInfoComponent.tsx
+++ src/components/PaymentPage/DeliverInfoComponent.tsx
@@ -2,39 +2,49 @@ import * as S from './DeliverInfoComponent.style';
 import { ReactComponent as EditIcon } from '../../assets/PaymentPage/edit.svg';
 
 interface AddressProps {
-    recipient: string;
-    phoneNumber: string;
-    postCode: string;
-    address: string;
-    addressDetail: string;
-    onEditAddress: () => void;
+  recipient: string;
+  phoneNumber: string;
+  postCode: string;
+  address: string;
+  addressDetail: string;
+  onEditAddress: () => void;
 }
 
-const DeliverInfoComponent = ({ recipient, phoneNumber, postCode, address, addressDetail, onEditAddress }: AddressProps) => {
-
-    return (
-        <S.Wrapper>
-            <S.TitleWrapper>
-                <S.Title>배송 정보</S.Title>
-                <S.Btn onClick={onEditAddress}><EditIcon /></S.Btn>
-            </S.TitleWrapper>
-            <S.Line></S.Line>
-            <S.InfoWrapper>
-                <S.TextWrapper>
-                    <S.IndexText>수령인</S.IndexText>
-                    <S.Text>{recipient}</S.Text>
-                </S.TextWrapper>
-                <S.TextWrapper>
-                    <S.IndexText>연락처</S.IndexText>
-                    <S.Text>{phoneNumber}</S.Text>
-                </S.TextWrapper>
-                <S.TextWrapper>
-                    <S.IndexText>주소</S.IndexText>
-                    <S.Text>[{postCode}] {address} {addressDetail}</S.Text>
-                </S.TextWrapper>
-            </S.InfoWrapper>
-        </S.Wrapper>
-    );
+const DeliverInfoComponent = ({
+  recipient,
+  phoneNumber,
+  postCode,
+  address,
+  addressDetail,
+  onEditAddress,
+}: AddressProps) => {
+  return (
+    <S.Wrapper>
+      <S.TitleWrapper>
+        <S.Title>배송 정보</S.Title>
+        <S.Btn onClick={onEditAddress}>
+          <EditIcon />
+        </S.Btn>
+      </S.TitleWrapper>
+      <S.Line></S.Line>
+      <S.InfoWrapper>
+        <S.TextWrapper>
+          <S.IndexText>수령인</S.IndexText>
+          <S.Text>{recipient}</S.Text>
+        </S.TextWrapper>
+        <S.TextWrapper>
+          <S.IndexText>연락처</S.IndexText>
+          <S.Text>{phoneNumber}</S.Text>
+        </S.TextWrapper>
+        <S.TextWrapper>
+          <S.IndexText>주소</S.IndexText>
+          <S.Text>
+            [{postCode}] {address} {addressDetail}
+          </S.Text>
+        </S.TextWrapper>
+      </S.InfoWrapper>
+    </S.Wrapper>
+  );
 };
 
 export default DeliverInfoComponent;
--- src/components/PaymentPage/ProductComponent.tsx
+++ src/components/PaymentPage/ProductComponent.tsx
@@ -1,37 +1,43 @@
 import * as S from './ProductComponent.style';
 
 interface ProductProps {
-    seller: string;
-    title: string;
-    price: number;
-    imageURL: string;
-    option: string;
+  seller: string;
+  title: string;
+  price: number;
+  imageURL: string;
+  option: string;
 }
 
-const ProductComponent = ({ seller, title, price, imageURL, option } : ProductProps) => {
-    return (
-        <S.Wrapper>
-            <S.Title>주문 상품</S.Title>
-            <S.ProductWrapper>
-                <S.Title>{seller}</S.Title>
-                <S.ContentWrapper>
-                    <S.ProductPicture imageURL={imageURL}></S.ProductPicture>
-                    <S.TextWrapper>
-                        <S.Title>{title}</S.Title>
-                        <S.OptionWrapper>
-                            <S.Option>{option}</S.Option>
-                            <S.Option>1개</S.Option>
-                        </S.OptionWrapper>
-                        {/* <S.OptionWrapper>
+const ProductComponent = ({
+  seller,
+  title,
+  price,
+  imageURL,
+  option,
+}: ProductProps) => {
+  return (
+    <S.Wrapper>
+      <S.Title>주문 상품</S.Title>
+      <S.ProductWrapper>
+        <S.Title>{seller}</S.Title>
+        <S.ContentWrapper>
+          <S.ProductPicture imageURL={imageURL}></S.ProductPicture>
+          <S.TextWrapper>
+            <S.Title>{title}</S.Title>
+            <S.OptionWrapper>
+              <S.Option>{option}</S.Option>
+              <S.Option>1개</S.Option>
+            </S.OptionWrapper>
+            {/* <S.OptionWrapper>
                             <S.Option>옵션2</S.Option>
                             <S.Option>{option2}개</S.Option>
                         </S.OptionWrapper> */}
-                        <S.Title>{price.toLocaleString()}원</S.Title>
-                    </S.TextWrapper>
-                </S.ContentWrapper>
-            </S.ProductWrapper>
-        </S.Wrapper>
-    );
+            <S.Title>{price.toLocaleString()}원</S.Title>
+          </S.TextWrapper>
+        </S.ContentWrapper>
+      </S.ProductWrapper>
+    </S.Wrapper>
+  );
 };
 
 export default ProductComponent;
